87936fcb4b76b6839d95f160317424279e1029df
Alex Kiprop <kipropalex59@gmail.com>
2019-10-20 11:17:56 +0300

    modified files

---
+++ instance/config.py
@@ -0,0 +1,2 @@
+MOVIE_API_KEY = 'dc440498b8f1b0aba9242cad5bb650e5'
+SECRET_KEY = '<Flask WTF Secret Key>'
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=dc440498b8f1b0aba9242cad5bb650e5
+export SECRET_KEY=Flask WTF Secret Key
+
+python3 manage.py server
